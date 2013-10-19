87bafbe69dd7270e3ca5fcde5bfbe85e08cb9d91
scharrier <scharrier@gmail.com>
2013-10-19 15:49:58 +0200

    Integration tests for Indices

---
+++ tests/integration/lib/Simples/Request/BaseTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Simples\Integration\Request ;
+
+use \Simples\Transport\Http ;
+
+abstract class BaseTest extends \PHPUnit_Framework_TestCase {
+
+	static $client ;
+
+	public static function setUpBeforeClass() {
+		static::$client = new Http([
+			'index' => 'twitter'
+		]) ;
+
+		// Creates the index and type used in most of the tests
+		static::$client->call('/twitter/tweet', 'POST') ;
+	}
+
+	public static function tearDownAfterClass() {
+		// Delete the test index
+		static::$client->call('/twitter', 'DELETE') ;
+	}
+}
---
+++ tests/integration/lib/Simples/Request/IndicesTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace Simples\Integration\Request ;
+
+require_once(__DIR__ . '/BaseTest.php') ;
+
+class IndicesTest extends BaseTest {
+
+	public function testCreate() {
+		static::$client->config(['index' => null]) ;
+		$this->assertTrue(static::$client->createIndex('test_create')->execute()->ok) ;
+
+		try {
+			static::$client->createIndex('test_create')->execute() ;
+			$this->fail() ;
+		} catch (\Exception $e) {}
+	}
+
+	public function testdelete() {
+		$this->assertTrue(static::$client->deleteIndex('test_create')->execute()->ok) ;
+
+		try {
+			static::$client->deleteIndex('test_create')->execute() ;
+			$this->fail() ;
+		} catch (\Exception $e) {}
+
+		static::$client->config(['index' => 'twitter']) ;
+	}
+}
