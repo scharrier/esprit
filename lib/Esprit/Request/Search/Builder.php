123d4a310dae5d8bd5f6bde99359072512ef99ec
scharrier <scharrier@gmail.com>
2013-10-28 13:55:12 +0100

    New builder.

--- lib/Esprit/Request/Search/Builder.php
+++ lib/Esprit/Request/Search/Builder.php
@@ -1,63 +1,7 @@
 <?php
+
 namespace Esprit\Request\Search ;
 
-/**
- * Search query builder.
- *
- * @author Sébastien Charrier <scharrier@gmail.com>
- * @package	Esprit
- * @subpackage Request.Search
- */
-abstract class Builder implements \Countable {
+class Builder {
 
-	use \Esprit\Behavior\DataContainer ;
-
-	/**
-	 * Request dependency.
-	 *
-	 * @var \Esprit\Request_Search
-	 */
-	protected $_request ;
-
-	/**
-	 * Constructor.
-	 *
-	 * @param mixed						$query		Query definition (string/array)
-	 * @param \Esprit\Request_Search	$request	Request calling this query builder.
-	 */
-	public function __construct(\Esprit\Request\Search $request = null) {
-		if (isset($request)) {
-			$this->_request = $request ;
-		}
-	}
-
-	/**
-	 * Default instance return : $this. Overriden in most builders.
-	 *
-	 * @return \Esprit\Request_Search_Builder this builder
-	 */
-	public function instance() {
-		return $this ;
-	}
-
-	/**
-	 * Add an element to the current builder.
-	 *
-	 * @param mixed		$criteria	Element to add.
-	 * @return mixed				Current builder instance or current request instance (fluid calls)
-	 */
-	abstract public function add($element, array $options = array()) ;
-
-
-	/**
-	 * Fluid interface : returns the request instance if set, or this instance.
-	 *
-	 * @return \Esprit_Base
-	 */
-	protected function _fluid() {
-		if (isset($this->_request)) {
-			return $this->_request ;
-		}
-		return $this ;
-	}
 }
