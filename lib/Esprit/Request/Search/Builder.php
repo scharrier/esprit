8a269945199f8b46d23d95849e9b03fc8fa3c99c
scharrier <scharrier@gmail.com>
2013-10-28 21:07:42 +0100

    First builder version. Really simple, but hey : it does the work !

--- lib/Esprit/Request/Search/Builder.php
+++ lib/Esprit/Request/Search/Builder.php
@@ -1,7 +1,43 @@
 <?php
-
 namespace Esprit\Request\Search ;
 
+/**
+ * Query builder.
+ */
 class Builder {
 
+	/**
+	 * Reflection classes (for performances)
+	 *
+	 * @var array
+	 */
+	static protected $_reflection = array() ;
+
+	/**
+	 * Static method doesn't exists : we try to instanciate.
+	 *
+	 * @param  string $class                     Class name
+	 * @param  array $args                       Arguments
+	 * @return \Esprit\Request\Search\Criteria   Search criteria if found
+	 */
+	static public function __callStatic($class, $args) {
+		$class = '\Esprit\Request\Search\Criteria\\' . ucfirst($class) ;
+		if (class_exists($class)) {
+			return static::_reflect($class)->newInstanceArgs($args);
+		}
+	}
+
+	/**
+	 * Generates (or returns if it exists) a reflection class for
+	 * the given classe name.
+	 *
+	 * @param string	$class		Class name
+	 * @return \ReflectionClass		Reflection class
+	 */
+	static protected function _reflect($class) {
+		if (!isset(static::$_reflection[$class])) {
+			static::$_reflection[$class] = new \ReflectionClass($class) ;
+		}
+		return static::$_reflection[$class] ;
+	}
 }
---
+++ tests/unit/lib/Esprit/Request/Search/BuilderTest.php
@@ -0,0 +1,47 @@
+<?php
+namespace Esprit\Request\Search ;
+
+use \Esprit\Request\Search\Builder as B ;
+
+class BuilderTest extends \PHPUnit_Framework_TestCase {
+
+	public function testSimpleCalls() {
+		$criteria = B::term('field','value') ;
+		$this->assertTrue($criteria instanceof \Esprit\Request\Search\Criteria\Term) ;
+		$this->assertEquals('value', $criteria->to('array')['term']['field']['value']) ;
+
+		$criteria = B::matchAll('field','value') ;
+		$this->assertTrue($criteria instanceof \Esprit\Request\Search\Criteria\MatchAll) ;
+	}
+
+	public function testComplexCalls() {
+		$criteria = B::should(
+			B::term('first_field','value'),
+			B::terms('second_field',['value_1','value_2']),
+			B::must(
+				B::matchAll(),
+				B::field(['in' => 'first_field','value' => '+select -that'])
+			),
+			['minimum_should_match' => 2]
+		) ;
+
+		$res = [
+			'bool' => [
+				'should' => [
+					['term' => ['first_field' => ['value' => 'value']]],
+					['terms' => ['second_field' => ['value_1', 'value_2']]],
+					['bool' => [
+						'must' => [
+							['match_all' => new \stdClass()],
+							['field' => ['first_field' => ['query' => '+select -that']]]]
+						]
+					]
+				],
+				'minimum_should_match' => 2
+			]
+		] ;
+
+		$this->assertEquals($criteria->to('array'), $res) ;
+
+	}
+}
